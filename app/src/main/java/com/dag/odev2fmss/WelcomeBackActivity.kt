51e2a91569035aafca62107945af347cf62797eb
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2022-09-02 19:28:27 +0300

    welcome back page add

--- app/src/main/java/com/dag/odev2fmss/WelcomeBackActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/WelcomeBackActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class WelcomeBackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_started)
+        setContentView(R.layout.activity_welcome_back)
     }
 }
