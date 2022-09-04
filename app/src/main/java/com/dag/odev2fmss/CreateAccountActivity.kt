d21c49f6c79e0d065f86014045ef65aeef2e55d3
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2022-09-04 15:50:30 +0300

    before input validation

--- app/src/main/java/com/dag/odev2fmss/CreateAccountActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/CreateAccountActivity.kt
@@ -1,11 +1,17 @@
 package com.dag.odev2fmss
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.dag.odev2fmss.databinding.ActivityCreateAccountBinding
 
 class CreateAccountActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateAccountBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_account)
     }
 }
--- app/src/main/java/com/dag/odev2fmss/WelcomeBackActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/WelcomeBackActivity.kt
@@ -1,11 +1,35 @@
 package com.dag.odev2fmss
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.dag.odev2fmss.databinding.ActivityWelcomeBackBinding
 
 class WelcomeBackActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBackBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_back)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome_back)
+
+        binding.createAccount.setOnClickListener(View.OnClickListener {
+
+            if(true){
+                val intent = Intent(this, CreateAccountActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
+
+
+
+    // function which checks all the text fields
+    // are filled or not by the user.
+    // when user clicks on the SIGN IN button
+    // this function is triggered.
+
+
 }
