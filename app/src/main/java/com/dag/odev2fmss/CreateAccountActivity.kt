11a771c02fd9fb96e0d8b1449c57b3f16b401f5a
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2022-09-04 20:03:58 +0300

    Add all logic

--- app/src/main/java/com/dag/odev2fmss/CreateAccountActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/CreateAccountActivity.kt
@@ -1,8 +1,12 @@
 package com.dag.odev2fmss
 
+import android.content.Intent
 import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.dag.odev2fmss.databinding.ActivityCreateAccountBinding
 
@@ -13,5 +17,86 @@ class CreateAccountActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_account)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create_account)
+        launcher()
+    }
+
+
+    private fun launcher() {
+        binding.createaccountBackIcon.setOnClickListener {
+            val intent = Intent(this, WelcomeBackActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent)
+        }
+
+        binding.createaccountSignup.setOnClickListener {
+            val username = binding.createaccountUsername.text
+            val password = binding.createaccountPassword.text
+            val email = binding.createaccountEmail.text
+
+            val isUserNameEmpty = username.isEmpty()
+            val isPasswordEmpty = password.isEmpty()
+            val isEmailEmpty = email.isEmpty()
+
+            if (isUserNameEmpty) {
+                binding.createaccountUsername.background.setTint(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.error
+                    )
+                )
+            } else {
+                binding.createaccountUsername.background.setTint(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.black
+                    )
+                )
+            }
+
+            if (isPasswordEmpty) {
+                binding.createaccountPassword.background.setTint(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.error
+                    )
+                )
+            } else {
+                binding.createaccountPassword.background.setTint(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.black
+                    )
+                )
+            }
+
+            if (isPasswordEmpty) {
+                binding.createaccountEmail.background.setTint(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.error
+                    )
+                )
+            } else {
+                binding.createaccountEmail.background.setTint(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.black
+                    )
+                )
+            }
+
+            if (isPasswordEmpty || isUserNameEmpty || isEmailEmpty) {
+                Toast.makeText(this, "Username, Email and Password not be empty", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Username: $username, Email: $email, Password: $password",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+        }
+
     }
 }
--- app/src/main/java/com/dag/odev2fmss/WelcomeBackActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/WelcomeBackActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.dag.odev2fmss.databinding.ActivityWelcomeBackBinding
 
@@ -14,22 +16,54 @@ class WelcomeBackActivity : AppCompatActivity() {
         setContentView(R.layout.activity_welcome_back)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome_back)
+        launcher()
 
-        binding.createAccount.setOnClickListener(View.OnClickListener {
+    }
 
-            if(true){
-                val intent = Intent(this, CreateAccountActivity::class.java)
-                startActivity(intent)
+    private fun launcher(){
+        //binding.welcomebackUsername.background.setTint(ContextCompat.getColor(this, R.color.error))
+        binding.backIcon.setOnClickListener {
+            val intent = Intent(this, GetStartedActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent)
+        }
+
+        binding.createAccount.setOnClickListener{
+            val intent = Intent(this, CreateAccountActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent)
+        }
+
+        binding.welcomebackLogin.setOnClickListener(View.OnClickListener {
+            val username = binding.welcomebackUsername.text
+            val password = binding.welcomebackPassword.text
+
+            val isUserNameEmpty = username.isEmpty()
+            val isPasswordEmpty = password.isEmpty()
+
+            if (isUserNameEmpty){
+                binding.welcomebackUsername.background.setTint(ContextCompat.getColor(this, R.color.error))
+            }else{
+                binding.welcomebackUsername.background.setTint(ContextCompat.getColor(this, R.color.black))
+            }
+
+            if(isPasswordEmpty){
+                binding.welcomebackPassword.background.setTint(ContextCompat.getColor(this, R.color.error))
+            }else{
+                binding.welcomebackPassword.background.setTint(ContextCompat.getColor(this, R.color.black))
+            }
+
+            if(isPasswordEmpty || isUserNameEmpty){
+                Toast.makeText(this,"Username and Password not be empty", Toast.LENGTH_LONG).show()
+            }else{
+                if (username.toString().equals(User.USERNAME) && password.toString().equals(User.PASSWORD)){
+                    Toast.makeText(this, "Welcome back $username", Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(this, "This user not exist", Toast.LENGTH_SHORT).show()
+                }
             }
         })
     }
-
-
-
-    // function which checks all the text fields
-    // are filled or not by the user.
-    // when user clicks on the SIGN IN button
-    // this function is triggered.
 
 
 }
